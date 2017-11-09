ff1178e90f04371d883635ccf01577a268fd955b
coderZsq <a13701777868@yahoo.com>
2017-11-09 16:45:42 +0800

    add

--- resume/src/main/java/com/resume/controller/PortalController.java
+++ resume/src/main/java/com/resume/controller/PortalController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by zhushuangquan on 08/11/2017.
  */
@@ -20,7 +22,8 @@ public class PortalController {
 
     @RequestMapping("fetch_profile.do")
     @ResponseBody
-    public ServerResponse fetchProfile() {
+    public ServerResponse fetchProfile(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         return iPortalService.fetchProfile();
     }
 }
