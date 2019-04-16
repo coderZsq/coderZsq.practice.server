6df5264ab5b8c81beca523a4f5bccb932ff798e1
coderZsq <a13701777868@yahoo.com>
2019-04-16 13:57:14 +0800

    add

---
+++ study-notes/algorithm/02-动态数组/src/com/coderZsq/Main.java
@@ -0,0 +1,22 @@
+package com.coderZsq;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = new int[] {11, 22, 33};
+
+        ArrayList list = new ArrayList();
+        list.clear();
+
+        // 1
+        // size++;
+        list.add(99);
+
+        // 2
+        // size++
+        list.add(88);
+
+        // list.get(2);
+        // list.set(2, element)
+    }
+}
