a0e66843c2d588c2e2770796e19fac4b9f0c8f4c
coderZsq <a13701777868@yahoo.com>
2019-04-24 17:37:33 +0800

    add

--- study-notes/algorithm/00-leetcode/src/栈/_150_逆波兰表达式求值.java
+++ study-notes/algorithm/00-leetcode/src/栈/_150_逆波兰表达式求值.java
@@ -1,11 +1,35 @@
 package 栈;
 
+import java.util.Stack;
+
 /**
  * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
  * @author zhushuangquan
  */
 public class _150_逆波兰表达式求值 {
     public int evalRPN(String[] tokens) {
-        return 0;
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    stack.push(- stack.pop() + stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    Integer second = stack.pop();
+                    Integer first = stack.pop();
+                    stack.push(first / second);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(s));
+                    break;
+            }
+        }
+        return stack.pop();
     }
 }
--- study-notes/algorithm/00-leetcode/src/栈/_856_括号的分数.java
+++ study-notes/algorithm/00-leetcode/src/栈/_856_括号的分数.java
@@ -1,11 +1,31 @@
 package 栈;
 
+import java.util.Stack;
+
 /**
  * https://leetcode-cn.com/problems/score-of-parentheses/
  * @author zhushuangquan
  */
 public class _856_括号的分数 {
     public int scoreOfParentheses(String S) {
-        return 0;
+        Stack<Character> stack = new Stack<>();
+        char[] c = S.toCharArray();
+        int sum = 0;
+        boolean f = true;
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '(') {
+                stack.add(c[i]);
+                f = true;
+            } else {
+                if (f) {
+                    sum += Math.pow(2, stack.size() - 1);
+                    stack.pop();
+                    f = false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return sum;
     }
 }
