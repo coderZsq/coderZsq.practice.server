284db2a012f129b95ac3f3f222ac898b81f504b7
coderZsq <a13701777868@yahoo.com>
2019-05-08 13:24:32 +0800

    add

--- study-notes/algorithm/07-AVL树/src/com/coderZsq/Main.java
+++ study-notes/algorithm/07-AVL树/src/com/coderZsq/Main.java
@@ -1,24 +1,102 @@
 package com.coderZsq;
 
 import com.coderZsq.printer.BinaryTrees;
+import com.coderZsq.time.Times;
 import com.coderZsq.tree.AVLTree;
+import com.coderZsq.tree.BST;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     static void test1() {
         Integer data[] = new Integer[] {
-            7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+                67, 52, 92, 96, 53, 95, 13, 63, 34, 82, 76, 54, 9, 68, 39
         };
 
         AVLTree<Integer> avl = new AVLTree<>();
         for (int i = 0; i < data.length; i++) {
             avl.add(data[i]);
+            System.out.println("[" + data[i] + "]");
+            BinaryTrees.println(avl);
+            System.out.println("------------------------------------------------------------------------------------------------------------------");
         }
 
-        BinaryTrees.println(avl);
+        for (int i = 0; i < data.length; i++) {
+            avl.remove(data[i]);
+            System.out.println("[" + data[i] + "]");
+            BinaryTrees.println(avl);
+            System.out.println("------------------------------------------------------------------------------------------------------------------");
+        }
+
+//        BinaryTrees.println(avl);
+    }
+
+    static void test2() {
+        List<Integer> data = new ArrayList<>();
+        for (int i = 0; i < 100_0000; i++) {
+            data.add((int)(Math.random() * 100_0000));
+        }
+
+        BST<Integer> bst = new BST<>();
+        Times.test("bst.add", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    bst.add(data.get(i));
+                }
+            }
+        });
+
+        Times.test("bst.contains", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    bst.contains(data.get(i));
+                }
+            }
+        });
+
+        Times.test("bst.remove", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    bst.remove(data.get(i));
+                }
+            }
+        });
+
+        AVLTree<Integer> avl = new AVLTree<>();
+        Times.test("avl.add", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    avl.add(data.get(i));
+                }
+            }
+        });
+
+        Times.test("avl.contains", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    avl.contains(data.get(i));
+                }
+            }
+        });
+
+        Times.test("avl.remove", new Times.Task() {
+            @Override
+            public void execute() {
+                for (int i = 0; i < data.size(); i++) {
+                    avl.remove(data.get(i));
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
-        test1();
+        test2();
     }
 }
