de47984c7dd6663e25b5266379991d546b75a903
coderZsq <a13701777868@yahoo.com>
2019-05-10 17:36:29 +0800

    add

--- study-notes/algorithm/08-红黑树/src/com/coderZsq/Main.java
+++ study-notes/algorithm/08-红黑树/src/com/coderZsq/Main.java
@@ -4,6 +4,7 @@ import com.coderZsq.printer.BinaryTrees;
 import com.coderZsq.time.Times;
 import com.coderZsq.tree.AVLTree;
 import com.coderZsq.tree.BST;
+import com.coderZsq.tree.RBTree;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -96,7 +97,22 @@ public class Main {
         });
     }
 
+    static void test3() {
+        Integer data[] = new Integer[] {
+                55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50
+        };
+
+        RBTree<Integer> rb = new RBTree<>();
+        for (int i = 0; i < data.length; i++) {
+            rb.add(data[i]);
+            System.out.println("[" + data[i] + "]");
+            BinaryTrees.println(rb);
+            System.out.println("------------------------------------------------------------------------------------------------------------------");
+        }
+//        BinaryTrees.println(rb);
+    }
+
     public static void main(String[] args) {
-        test1();
+        test3();
     }
 }
--- study-notes/algorithm/08-红黑树/src/com/coderZsq/tree/AVLTree.java
+++ study-notes/algorithm/08-红黑树/src/com/coderZsq/tree/AVLTree.java
@@ -4,7 +4,7 @@ package com.coderZsq.tree;
 import java.util.Comparator;
 
 @SuppressWarnings("unchecked")
-public class AVLTree<E> extends BST<E> {
+public class AVLTree<E> extends BBST<E> {
     public AVLTree() {
         this(null);
     }
@@ -29,7 +29,7 @@ public class AVLTree<E> extends BST<E> {
     }
 
     @Override
-    protected void afterRemove(Node<E> node) {
+    protected void afterRemove(Node<E> node, Node<E> replacement) {
         while ((node = node.parent) != null) {
             if (isBalanced(node)) {
                 // 更新高度
@@ -50,7 +50,7 @@ public class AVLTree<E> extends BST<E> {
      * 恢复平衡
      * @param grand 高度最低的那个不平衡节点
      * */
-    private void rebalance(Node<E> grand) {
+    private void rebalance2(Node<E> grand) {
         Node<E> parent = ((AVLNode<E>) grand).tallerChild();
         Node<E> node = ((AVLNode<E>) parent).tallerChild();
         if (parent.isLeftChild()) { // L
@@ -70,11 +70,30 @@ public class AVLTree<E> extends BST<E> {
         }
     }
 
+    @Override
+    protected void afterRotate(Node<E> grand, Node<E> parent, Node<E> child) {
+        super.afterRotate(grand, parent, child);
+
+        // 更新高度
+        updateHeight(grand);
+        updateHeight(parent);
+    }
+
+    @Override
+    protected void rotate(Node<E> r, Node<E> b, Node<E> c, Node<E> d, Node<E> e, Node<E> f) {
+        super.rotate(r, b, c, d, e, f);
+
+        // 更新高度
+        updateHeight(b);
+        updateHeight(f);
+        updateHeight(d);
+    }
+
     /**
      * 恢复平衡
      * @param grand 高度最低的那个不平衡节点
      * */
-    private void rebalance2(Node<E> grand) {
+    private void rebalance(Node<E> grand) {
         Node<E> parent = ((AVLNode<E>) grand).tallerChild();
         Node<E> node = ((AVLNode<E>) parent).tallerChild();
         if (parent.isLeftChild()) { // L
@@ -90,91 +109,6 @@ public class AVLTree<E> extends BST<E> {
                 rotate(grand, grand, parent.left, parent, node.left, node);
             }
         }
-    }
-
-    private void rotate(
-            Node<E> r, // 子树的根节点
-            Node<E> b, Node<E> c,
-            Node<E> d,
-            Node<E> e, Node<E> f) {
-        // 让d成为这颗子树的根节点
-        d.parent = r.parent;
-        if (r.isLeftChild()) {
-            r.parent.left = d;
-        } else if (r.isRightChild()) {
-            r.parent.right = d;
-        } else {
-            root = d;
-        }
-
-        // b-c
-//        b.left = a;
-//        if (a != null) {
-//            a.parent = b;
-//        }
-        b.right = c;
-        if (c != null) {
-            c.parent = b;
-        }
-        updateHeight(b);
-
-        // e-f
-        f.left = e;
-        if (e != null) {
-            e.parent = f;
-        }
-//        f.right = g;
-//        if (g != null) {
-//            g.parent = f;
-//        }
-        updateHeight(f);
-
-        // b-d-f
-        d.left = b;
-        d.right = f;
-        b.parent = d;
-        f.parent = d;
-        updateHeight(d);
-    }
-
-    private void rotateLeft(Node<E> grand) {
-        Node<E> parent = grand.right;
-        Node<E> child = parent.left;
-        grand.right = child;
-        parent.left = grand;
-        afterRotate(grand, parent, child);
-    }
-
-    private void rotateRight(Node<E> grand) {
-        Node<E> parent = grand.left;
-        Node<E> child = parent.right;
-        grand.left = child;
-        parent.right = grand;
-        afterRotate(grand, parent, child);
-    }
-
-    private void afterRotate(Node<E> grand, Node<E> parent, Node<E> child) {
-        // 让parent成为子树的根节点
-        parent.parent = grand.parent;
-        if (grand.isLeftChild()) {
-            grand.parent.left = parent;
-        } else if (grand.isRightChild()) {
-            grand.parent.right = parent;
-        } else { // grand是root节点
-            root = parent;
-        }
-
-        // 更新child的parent
-        if (child != null) {
-            child.parent = grand;
-        }
-
-        // 更新grand的parent
-        grand.parent = parent;
-
-        // 更新高度
-        updateHeight(grand);
-        updateHeight(parent);
     }
 
     private boolean isBalanced(Node<E> node) {
