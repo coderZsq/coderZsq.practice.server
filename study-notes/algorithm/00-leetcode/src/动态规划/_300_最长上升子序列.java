34edf80802fd207e93df8d63043354bf2f3170f5
coderZsq <a13701777868@yahoo.com>
2019-11-18 13:00:10 +0800

    add

---
+++ study-notes/algorithm/00-leetcode/src/动态规划/_300_最长上升子序列.java
@@ -0,0 +1,22 @@
+package 动态规划;
+
+/**
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/
+ * @author zhushuangquan
+ */
+public class _300_最长上升子序列 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int max = dp[0] = 1;
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] <= nums[j]) continue;
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
---
+++ study-notes/algorithm/00-leetcode/src/动态规划/_53_最大子序和.java
@@ -0,0 +1,50 @@
+package 动态规划;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-subarray/
+ * @author zhushuangquan
+ */
+public class _53_最大子序和 {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int dp = nums[0];
+        int max = dp;
+        for (int i = 1; i < nums.length; i++) {
+            if (dp <= 0) {
+                dp = nums[i];
+            } else {
+                dp = dp + nums[i];
+            }
+            max = Math.max(dp, max);
+        }
+        return max;
+    }
+
+    public int maxSubArray1(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        return maxSubArray1(nums, 0, nums.length);
+    }
+
+    static int maxSubArray1(int[] nums, int begin, int end) {
+        if (end - begin < 2) return nums[begin];
+        int mid = (begin + end) >> 1;
+        int leftMax = Integer.MIN_VALUE;
+        int leftSum = 0;
+        for (int i = mid - 1; i >= begin; i--) {
+            leftSum += nums[i];
+            leftMax = Math.max(leftMax, leftSum);
+        }
+
+        int rightMax = Integer.MIN_VALUE;
+        int rightSum = 0;
+        for (int i = mid; i < end; i++) {
+            rightSum += nums[i];
+            rightMax = Math.max(rightMax, rightSum);
+        }
+        return Math.max(leftMax + rightMax,
+                Math.max(
+                        maxSubArray1(nums, begin, mid),
+                        maxSubArray1(nums, mid, end))
+        );
+    }
+}
