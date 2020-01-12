41675ddc319ec49630705b346b964543c8185e02
coderZsq <a13701777868@yahoo.com>
2020-01-12 21:13:24 +0800

    add

---
+++ study-notes/blockchain/13-cli-send/blc/utils.go
@@ -0,0 +1,17 @@
+package blc
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
+// IntToHex 实现int64转[]byte
+func IntToHex(data int64) []byte {
+	buffer := new(bytes.Buffer)
+	err := binary.Write(buffer, binary.BigEndian, data)
+	if nil != err {
+		log.Panicf("int transact to []byte failed! %v\n", err)
+	}
+	return buffer.Bytes()
+}
