4bc4c12f494d6978284f4b717326c9aa73f77524
coderZsq <a13701777868@yahoo.com>
2020-01-16 11:42:02 +0800

    add

--- study-notes/j2ee-collection/javaweb/02-MySQL基础/src/com/coderZsq/dql/SELECT.sql
+++ study-notes/j2ee-collection/javaweb/02-MySQL基础/src/com/coderZsq/dql/SELECT.sql
@@ -24,3 +24,83 @@ SELECT id, productName, (costPrice + 1) * 50 allPrice FROM product
 # 需求: 查询商品的名字和零售价.
 #     格式: xxx商品的零售价为: xxx
 SELECT CONCAT (productName, '商品的零售价为: ', salePrice) AS productSalePrice FROM product
+# 运算符   含义
+# -----------------------------------------------
+# =       等于
+# >       大于
+# >=      大于或等于
+# <       小于
+# <=      小于或等于
+# != (<>) 不等于
+# -----------------------------------------------
+# 需求: 查询货品零售价为119的所有货品信息
+SELECT * FROM product WHERE salePrice = 119
+# 需求: 查询货品名为罗技G9X的所有货品信息
+SELECT * FROM product WHERE productName = '罗技G9X'
+# 需求: 查询货品名 不为 罗技G9X的所有货品信息
+SELECT * FROM product WHERE productName != '罗技G9X'
+# 需求: 查询分类编号不等于2的货品信息
+SELECT * FROM product WHERE dir_id <> 2
+# 需求: 查询货品名称, 零售价小于等于200的货品
+SELECT productName, salePrice FROM product WHERE salePrice <= 200
+# 需求: 查询id, 货品名称, 批发价大于350的货品
+SELECT id, productName, salePrice * cutoff
+FROM product
+WHERE salePrice * cutoff > 350
+# 思考: 使用where后面使用别名不行, 总结select和where的执行顺序
+# -----------------------------------------------
+# 运算符    含义
+# -----------------------------------------------
+# AND (&&) 如果组合的条件都是TRUE, 返回TRUE
+# OR (||)  如果组合的条件之一都是TRUE, 返回TRUE
+# NOT (!)  如果下面的条件时FALSE, 返回TRUE
+# -----------------------------------------------
+# 需求: 选择id, 货品名称, 零售价在300-400之间的货品
+SELECT id, productName, salePrice FROM product
+WHERE salePrice >= 300 AND salePrice <= 400
+# 需求: 选择id, 货品名称, 分类编号为2, 4的所有货品
+SELECT * FROM product WHERE dir_id = 2 OR dir_id = 4
+# 需求: 选择id, 货品名称, 分类编号不为2的所有商品
+SELECT id, productName, dir_id FROM product WHERE dir_id != 2
+SELECT id, productName, dir_id FROM product WHERE NOT dir_id = 2
+# 需求: 选择id, 分类编号的货品零售价大于等于250或者是成本大于等于200
+SELECT id, productName, dir_id, salePrice, costPrice
+FROM product
+WHERE salePrice >= 250 OR costPrice >= 200
+
+SELECT id, productName FROM product
+WHERE
+    (NOT productName LIKE '%M%' AND salePrice > 100) OR (dir_id = 2)
+# -----------------------------------------------
+# WHERE 列名 BETWEEN 最小值 AND 最大值: 闭区间
+# 需求: 选择id, 货品名称, 零售价在300-400之间的货品
+SELECT id, productName, salePrice FROM product
+WHERE salePrice >= 300 AND salePrice <= 400
+SELECT id, productName, salePrice FROM product
+WHERE salePrice BETWEEN 300 AND 400
+# 需求: 选择id, 货品名称, 零售价不在300-400之间的货品
+SELECT id, productName, salePrice FROM product
+WHERE NOT (salePrice >= 300 AND salePrice <= 400)
+SELECT id, productName, salePrice FROM product
+WHERE  salePrice NOT BETWEEN 300 AND 400
+# 需求: 选择id, 货品名称, 分类编号为2, 4的所有货品
+SELECT * FROM product WHERE dir_id = 2 OR dir_id = 4
+SELECT * FROM product WHERE dir_id IN (2, 4)
+# 需求: 选择id, 货品名称, 分类编号不为2, 4的所有货品
+SELECT * FROM product WHERE dir_id NOT IN (2, 4)
+# IS NULL: 判断列的值是否为空
+# 格式: WHERE 列名 IS NULL
+# 需求: 查询商品名为NULL的所有商品信息
+SELECT * FROM product WHERE productName IS NULL
+# -----------------------------------------------
+# 使用LIKE运算符执行通配查询, 查询条件可包含文字字符或数字
+# %通配符: 可表示零或多个字符
+# _通配符: 可表示一个字符
+# 通配符: 用来实现匹配部分值的特殊字符
+# -----------------------------------------------
+# 需求: 查询id, 货品名称, 货品名称匹配'%罗技M9_'
+SELECT * FROM product WHERE productName LIKE '%罗技M9_'
+SELECT * FROM product WHERE productName LIKE '%罗技M9%'
+# 需求: 查询id, 货品名称, 分类编号, 零售价大于等于80并且货品名称匹配'%罗技M1__'
+SELECT id, productName, dir_id, salePrice FROM product
+WHERE salePrice >= 80 AND productName LIKE '%罗技M1__'
