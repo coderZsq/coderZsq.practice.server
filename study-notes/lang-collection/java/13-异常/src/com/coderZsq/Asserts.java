cdcf79bace68344d68245f245d43d11221660aa1
coderZsq <a13701777868@yahoo.com>
2020-01-19 14:55:10 +0800

    add

--- study-notes/lang-collection/java/13-异常/src/com/coderZsq/Asserts.java
+++ study-notes/lang-collection/java/13-异常/src/com/coderZsq/Asserts.java
@@ -6,10 +6,11 @@ package com.coderZsq;
 public class Asserts {
     public static void test(boolean v) {
         if (v) return;
-        try {
-            throw new IllegalArgumentException("条件不成立");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.err.println(new RuntimeException().getStackTrace()[1]);
+//        try {
+//            throw new IllegalArgumentException("条件不成立");
+//        } catch (Exception e) {
+//            System.err.println(e.getStackTrace()[1]);
+//        }
     }
 }
