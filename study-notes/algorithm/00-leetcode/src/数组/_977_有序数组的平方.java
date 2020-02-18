9f156c4ae006a194e41462165e9a215c0bb417a3
coderZsq <a13701777868@yahoo.com>
2020-02-18 18:43:35 +0800

    add

--- study-notes/algorithm/00-leetcode/src/数组/_977_有序数组的平方.java
+++ study-notes/algorithm/00-leetcode/src/数组/_977_有序数组的平方.java
@@ -6,7 +6,25 @@ package 数组;
  * @author zhushuangquan
  */
 public class _977_有序数组的平方 {
-    public static int[] sortedSquares(int[] A) {
+    public int[] sortedSquares(int[] A) {
+        int l = 0, r = A.length - 1;
+        int cur = r;
+        int[] B = new int[A.length];
+        while (l <= r) {
+            int lv = A[l] * A[l];
+            int rv = A[r] * A[r];
+            if (lv > rv) {
+                B[cur--] = lv;
+                l++;
+            } else {
+                B[cur--] = rv;
+                r--;
+            }
+        }
+        return B;
+    }
+
+    public int[] sortedSquares1(int[] A) {
         if (A.length == 0) return new int[]{};
         int p = 0;
         for (int i = 0; i < A.length; i++) {
@@ -15,28 +33,28 @@ public class _977_有序数组的平方 {
                 break;
             }
         }
-        int[] newA = new int[A.length];
+        int[] B = new int[A.length];
         int l = 0;
         int r = A.length - 1;
-        int i = newA.length - 1;
+        int i = B.length - 1;
         while (r >= p && l < p) {
             if (Math.abs(A[l]) > A[r]) {
-                newA[i--] = (int) Math.pow(A[l++], 2);
+                B[i--] = (int) Math.pow(A[l++], 2);
             } else {
-                newA[i--] = (int) Math.pow(A[r--], 2);
+                B[i--] = (int) Math.pow(A[r--], 2);
             }
         }
         if (i >= 0) {
             if (l < p) {
                 for (int j = l; j < p; j++) {
-                    newA[i--] = (int) Math.pow(A[j], 2);
+                    B[i--] = (int) Math.pow(A[j], 2);
                 }
             } else {
                 for (int j = r; j >= p; j--) {
-                    newA[i--] = (int) Math.pow(A[j], 2);
+                    B[i--] = (int) Math.pow(A[j], 2);
                 }
             }
         }
-        return newA;
+        return B;
     }
 }
