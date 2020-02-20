8c0bdd2c7f5d65cc9ab9d656ca14c12d190e99fb
coderZsq <a13701777868@yahoo.com>
2020-02-20 12:22:21 +0800

    add

---
+++ study-notes/algorithm/00-leetcode/src/链表/_160_相交链表.java
@@ -0,0 +1,17 @@
+package 链表;
+
+/**
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ * @author zhushuangquan
+ */
+public class _160_相交链表 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode curA = headA, curB = headB;
+        while (curA != curB) {
+            curA = (curA == null) ? headB : curA.next;
+            curB = (curB == null) ? headA : curB.next;
+        }
+        return curA;
+    }
+}
---
+++ study-notes/algorithm/00-leetcode/src/链表/_面试题52_两个链表的第一个公共节点.java
@@ -0,0 +1,17 @@
+package 链表;
+
+/**
+ * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+ * @author zhushuangquan
+ */
+public class _面试题52_两个链表的第一个公共节点 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode curA = headA, curB = headB;
+        while (curA != curB) {
+            curA = (curA == null) ? headB : curA.next;
+            curB = (curB == null) ? headA : curB.next;
+        }
+        return curA;
+    }
+}
