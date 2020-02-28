6138a18db61d4724eab96f353771033ae29f71ef
coderZsq <a13701777868@yahoo.com>
2020-02-28 12:18:55 +0800

    add

---
+++ study-notes/android/UI/app/src/main/java/com/coderZsq/ui/UnitTestUtils.java
@@ -0,0 +1,13 @@
+package com.coderZsq.ui;
+
+public class UnitTestUtils {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int mul(int x, int y) {
+        return x * y;
+    }
+    public String getString() {
+        return "哈哈哈";
+    }
+}
