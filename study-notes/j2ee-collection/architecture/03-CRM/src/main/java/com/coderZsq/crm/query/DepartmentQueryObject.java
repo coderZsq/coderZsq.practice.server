3df86ed61211f084a5e2e14d84cb6c00e04b68ab
coderZsq <a13701777868@yahoo.com>
2020-03-17 12:35:38 +0800

    add

--- study-notes/j2ee-collection/architecture/03-CRM/src/main/java/com/coderZsq/crm/query/DepartmentQueryObject.java
+++ study-notes/j2ee-collection/architecture/03-CRM/src/main/java/com/coderZsq/crm/query/DepartmentQueryObject.java
@@ -7,4 +7,8 @@ public class DepartmentQueryObject {
     private String keyword;
     private Integer currentPage = 1; // 当前页
     private Integer pageSize = 5; // 每页数量
+
+    public String getKeyword() {
+        return keyword == null || keyword.trim().length() == 0 ? null : keyword;
+    }
 }
