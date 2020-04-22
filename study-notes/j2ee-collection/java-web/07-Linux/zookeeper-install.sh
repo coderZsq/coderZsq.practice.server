5ee7447d98aac01b0198ca22371cd3d7a410e74c
coderZsq <a13701777868@yahoo.com>
2020-04-22 14:29:29 +0800

    add

--- study-notes/j2ee-collection/java-web/07-Linux/zookeeper-install.sh
+++ study-notes/j2ee-collection/java-web/07-Linux/zookeeper-install.sh
@@ -158,3 +158,79 @@ Created /pNode/node0000000002
 # cnode
 [zk: localhost:2181(CONNECTED) 8] get /cnode # 1分钟后如果容器节点中没有子节点, 自动删除该节点, znode.container.checkIntervalMs
 # org.apache.zookeeper.KeeperException$NoNodeException: KeeperErrorCode = NoNode for /cnode
+
+# 查看节点
+[zk: localhost:2181(CONNECTED) 9] ls -R / # -R 递归查看
+# /
+# /pNode
+# /zookeeper
+# /pNode/node0000000001
+# /pNode/node0000000002
+# /pNode/node01
+# /zookeeper/config
+# /zookeeper/quota
+[zk: localhost:2181(CONNECTED) 10] ls -s /pNode # -s 查看状态信息
+# [node0000000001, node0000000002, node01]
+# cZxid = 0x100000002
+# ctime = Wed Apr 22 13:41:53 CST 2020
+# mZxid = 0x100000002
+# mtime = Wed Apr 22 13:41:53 CST 2020
+# pZxid = 0x100000006
+# cversion = 3
+# dataVersion = 0
+# aclVersion = 0
+# ephemeralOwner = 0x0
+# dataLength = 0
+# numChildren = 3
+
+# 删除节点
+[zk: localhost:2181(CONNECTED) 12] delete -v 1 /pNode
+# version No is not valid : /pNode
+[zk: localhost:2181(CONNECTED) 13] delete -v 3 /pNode
+# version No is not valid : /pNode
+[zk: localhost:2181(CONNECTED) 14] deleteall /pNode
+[zk: localhost:2181(CONNECTED) 15] ls /
+# [zookeeper]
+
+# 绑定事件
+[zk: localhost:2181(CONNECTED) 17] printwatches on
+[zk: localhost:2181(CONNECTED) 18] create /vNode
+# Created /vNode
+[zk: localhost:2181(CONNECTED) 19] ls -w /vNode
+# []
+[zk: localhost:2181(CONNECTED) 20] set /vNode abc
+[zk: localhost:2181(CONNECTED) 21] create /vNode/node1
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeChildrenChanged path:/vNode
+# Created /vNode/node1
+[zk: localhost:2181(CONNECTED) 22] create /vNode/abc
+# Node already exists: /vNode/abc  
+[zk: localhost:2181(CONNECTED) 23] get -w /vNode
+# abc
+[zk: localhost:2181(CONNECTED) 24] set /vNode bbc
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeDataChanged path:/vNode
+
+[zk: localhost:2181(CONNECTED) 25] addWatch /vNode
+[zk: localhost:2181(CONNECTED) 26] create /vNode/node4
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeCreated path:/vNode/node4
+# Created /vNode/node4
+[zk: localhost:2181(CONNECTED) 27] create /vNode/node5
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeCreated path:/vNode/node5
+# Created /vNode/node5
+[zk: localhost:2181(CONNECTED) 28] set /vNode/node5 node5
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeDataChanged path:/vNode/node5
+
+[zk: localhost:2181(CONNECTED) 29] create /eNode
+# Created /eNode
+[zk: localhost:2181(CONNECTED) 31] addWatch -m PERSISTENT /eNode
+[zk: localhost:2181(CONNECTED) 32] set /eNode enode
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeDataChanged path:/eNode
+[zk: localhost:2181(CONNECTED) 33] create /eNode/node1
+# WATCHER::
+# WatchedEvent state:SyncConnected type:NodeChildrenChanged path:/eNode
+# Created /eNode/node1
