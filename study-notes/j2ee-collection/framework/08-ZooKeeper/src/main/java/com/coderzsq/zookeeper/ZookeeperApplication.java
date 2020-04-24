debd19b27f33277a8018f6cf2a2281909aa4c54f
coderZsq <a13701777868@yahoo.com>
2020-04-24 18:10:29 +0800

    add

---
+++ study-notes/j2ee-collection/framework/08-ZooKeeper/src/main/java/com/coderzsq/zookeeper/ZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.coderzsq.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperApplication.class, args);
+    }
+
+}
