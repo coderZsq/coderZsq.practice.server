73c73ba69b7121ac4be32604e486b0144416263e
coderZsq <a13701777868@yahoo.com>
2020-05-11 18:37:22 +0800

    add

--- study-notes/j2ee-collection/framework/09-Redis/src/test/java/com/coderZsq/redis/_05_hyperloglog/HyperLogLogDemo.java
+++ study-notes/j2ee-collection/framework/09-Redis/src/test/java/com/coderZsq/redis/_05_hyperloglog/HyperLogLogDemo.java
@@ -4,7 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
 import redis.clients.jedis.Pipeline;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class HyperLogLogDemo {
     private Jedis jedis = null; // 客户端连接对象
@@ -49,5 +54,30 @@ public class HyperLogLogDemo {
             pipelined.set("key" + i, "value" + i);
         }
         pipelined.sync();
+    }
+
+    @Test
+    public void testFailover() throws Exception {
+        String masterName = "mymaster";
+        Set<String> sentinels = new HashSet<>();
+        sentinels.add("172.16.21.175:26379");
+        sentinels.add("172.16.21.175:26380");
+        sentinels.add("172.16.21.175:26381");
+        JedisSentinelPool pool = new JedisSentinelPool(masterName, sentinels);
+        while (true) {
+            Jedis jedis = null;
+            try {
+                jedis = pool.getResource();
+                jedis.incr("age");
+                System.out.println("age = " + jedis.get("age"));
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                if (jedis != null) {
+                    jedis.close();
+                }
+            }
+        }
     }
 }
--- study-notes/j2ee-collection/java-web/07-Linux/redis-install.sh
+++ study-notes/j2ee-collection/java-web/07-Linux/redis-install.sh
@@ -51,7 +51,8 @@ cat data/6379.log
 /usr/local/redis-5.0.8/bin/redis-cli shutdown
 /usr/local/redis-5.0.8/bin/redis-cli -a ssh-keygen shutdown
 
-# 分布式
+# 主从复制
+# 1. ========命令行========
 cat redis_6379.conf |grep -v '#' |grep -v '^$'
 
 vi redis_6379.conf
@@ -67,3 +68,157 @@ vi redis_6379.conf
 cp redis_6379.conf redis_6380.conf
 vi redis_6380.conf
 :%s/6379/6380
+
+bin/redis-server conf/redis_6379.conf
+bin/redis-server conf/redis_6380.conf
+bin/redis-cli
+bin/redis-cli -p 6380
+
+# 127.0.0.1:6379> info replication
+# # Replication
+# role:master
+# connected_slaves:0
+# master_replid:02c7e54fd2851069f87ed54771134dece3f40954
+# master_replid2:0000000000000000000000000000000000000000
+# master_repl_offset:0
+# second_repl_offset:-1
+# repl_backlog_active:0
+# repl_backlog_size:1048576
+# repl_backlog_first_byte_offset:0
+# repl_backlog_histlen:0
+
+SLAVEOF 127.0.0.1 6379
+
+# 127.0.0.1:6380> info replication
+# # Replication
+# role:slave
+# master_host:127.0.0.1
+# master_port:6379
+# master_link_status:up
+# master_last_io_seconds_ago:4
+# master_sync_in_progress:0
+# slave_repl_offset:225
+# slave_priority:100
+# slave_read_only:1
+# connected_slaves:0
+# master_replid:5553e504b01d2a18ddc55199b577fbd2881c639d
+# master_replid2:0000000000000000000000000000000000000000
+# master_repl_offset:225
+# second_repl_offset:-1
+# repl_backlog_active:1
+# repl_backlog_size:1048576
+# repl_backlog_first_byte_offset:1
+# repl_backlog_histlen:225
+
+SLAVEOF NO ONE
+
+# 127.0.0.1:6380> info replication
+# # Replication
+# role:master
+# connected_slaves:0
+# master_replid:4971b37aea0ab49052d5973a01d38da9356fc5a3
+# master_replid2:5553e504b01d2a18ddc55199b577fbd2881c639d
+# master_repl_offset:365
+# second_repl_offset:366
+# repl_backlog_active:1
+# repl_backlog_size:1048576
+# repl_backlog_first_byte_offset:1
+# repl_backlog_histlen:365
+
+# 2. =======配置========
+vi redis_6379.conf
+# bind 172.16.21.175
+
+vi redis_6380.conf
+# bind 172.16.21.175
+# replicaof 172.16.21.175 6379
+bin/redis-cli -p 6380 shutdown
+bin/redis-server conf/redis_6380.conf
+bin/redis-cli -p 6380
+
+bin/redis-server conf/redis_6379.conf
+bin/redis-server conf/redis_6380.conf
+bin/redis-cli -p 6380 -h 172.16.21.175
+
+# Sentinel
+# 配置开启主从节点
+vi redis_7000.conf
+# port 7000
+# daemonize yes
+# pidfile /var/run/redis-7000.pid
+# logfile 7000.log
+# dir /usr/local/redis-5.0.8/data
+# bind 172.16.21.175
+
+vi redis_7001.conf
+# port 7001
+# daemonize yes
+# pidfile /var/run/redis-7001.pid
+# logfile 7001.log
+# dir /usr/local/redis-5.0.8/data
+# bind 172.16.21.175
+# replicaof 172.16.21.175 7000
+
+vi redis_7002.conf
+# port 7002
+# daemonize yes
+# pidfile /var/run/redis-7002.pid
+# logfile 7002.log
+# dir /usr/local/redis-5.0.8/data
+# bind 172.16.21.175
+# replicaof 172.16.21.175 7000
+
+bin/redis-server conf/redis_7000.conf
+bin/redis-server conf/redis_7001.conf
+bin/redis-server conf/redis_7002.conf
+
+# 配置开启sentinel监控主节点
+vi sentinel_26379.conf
+# port 26379
+# daemonize yes
+# pidfile /var/run/redis-sentinel-26379.pid
+# logfile "26379.log"
+# dir /usr/local/redis-5.0.8/data/
+# sentinel monitor mymaster 172.16.21.175 7000 2
+# sentinel down-after-milliseconds mymaster 30000
+# sentinel parallel-syncs mymaster 1
+# sentinel failover-timeout mymaster 180000
+# sentinel deny-scripts-reconfig yes
+# protected-mode no
+# bind 172.16.21.175
+
+vi sentinel_26380.conf
+# port 26380
+# daemonize yes
+# pidfile /var/run/redis-sentinel-26380.pid
+# logfile "26380.log"
+# dir /usr/local/redis-5.0.8/data/
+# sentinel monitor mymaster 172.16.21.175 7000 2
+# sentinel down-after-milliseconds mymaster 30000
+# sentinel parallel-syncs mymaster 1
+# sentinel failover-timeout mymaster 180000
+# sentinel deny-scripts-reconfig yes
+# protected-mode no
+# bind 172.16.21.175
+
+vi sentinel_26381.conf
+# port 26381
+# daemonize yes
+# pidfile /var/run/redis-sentinel-26381.pid
+# logfile "26381.log"
+# dir /usr/local/redis-5.0.8/data/
+# sentinel monitor mymaster 172.16.21.175 7000 2
+# sentinel down-after-milliseconds mymaster 30000
+# sentinel parallel-syncs mymaster 1
+# sentinel failover-timeout mymaster 180000
+# sentinel deny-scripts-reconfig yes
+# protected-mode no
+# bind 172.16.21.175
+
+# 启动哨兵
+bin/redis-sentinel conf/sentinel_26379.conf
+bin/redis-sentinel conf/sentinel_26380.conf
+bin/redis-sentinel conf/sentinel_26381.conf
+
+# 模拟单点故障
+bin/redis-cli -h 172.16.21.175 -p 7000 shutdown
