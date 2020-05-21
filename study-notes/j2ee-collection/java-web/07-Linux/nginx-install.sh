e26361086f7e710fbe1433e83bf08df06c28584c
coderZsq <a13701777868@yahoo.com>
2020-05-21 14:08:40 +0800

    add

--- study-notes/j2ee-collection/java-web/07-Linux/nginx-install.sh
+++ study-notes/j2ee-collection/java-web/07-Linux/nginx-install.sh
@@ -292,3 +292,165 @@ http {
   }
 }
 
+# 高可用环境搭建
+# 1. 安装keepalived依赖的包
+yum install gcc openssl-devel libnl3-devel popt-devel iptables-devel libnfnetlink-devel -y
+
+# 2. 编译安装keepalived
+tar -zxvf keepalived-2.0.20.tar.gz -C /usr/local/src
+cd /usr/local/src/keepalived-2.0.20/
+./configure --prefix=/usr/local/keepalived
+make && make install
+
+# 3. 将keepalived安装成Linux系统服务
+mkdir /etc/keepalived
+cp /usr/local/keepalived/etc/keepalived/keepalived.conf /etc/keepalived/
+cp /usr/local/keepalived/sbin/keepalived /usr/sbin/
+cp /usr/local/keepalived/etc/sysconfig/keepalived /etc/sysconfig/
+cp /usr/local/keepalived/sbin/keepalived /etc/init.d/
+
+# 4.编写nginx检测脚本
+vi /etc/keepalived/nginx_check.sh
+# #!/bin/bash
+# A=`ps -C nginx -- no-header |wc -l`
+# if [ $A -eq 0 ]; then
+#   /usr/local/nginx/sbin/nginx
+#   sleep 2
+#   if [ `ps -C nginx --no-header |wc -l` -eq 0 ];then
+#     killall keepalived
+#   fi
+# fi
+
+# 5. 添加执行权限
+chmod +x /etc/keepalived/nginx_check.sh
+
+# 6. 修改keepalived的Master配置文件:
+vi /etc/keepalived/keepalived.conf
+# :1,$d
+
+# !Configuration File for keepalived
+# global_defs {
+#   router_id coderZsq #路由器标志
+# }
+# #集群资源监控, 组合track_script进行
+# vrrp_script check_haproxy {
+#   script "/etc/keepalived/nginx_check.sh" #检测 nginx 状态的脚本路径
+#   interval 2 #检测时间间隔
+#   weight -20 #条件成立 权重减20
+# }
+# vrrp_instance PROXY {
+#   #设置当前主机为主节点, 如果是备用节点, 则设置为BACKUP
+#   state MASTER
+#   #指定HA监测网络接口, 可以用ifconfig查看来决定设置哪一个 ens32 / eth0
+#   interface eth0
+#   #虚拟路由标识, 同一个VRRP实例要使用同一个标识, 主备机
+#   virtual_router_id 80
+#   #因为当前环境中VRRP组播有问题, 改为使用单播发送VRRP报文 如果VRRP组播没问题, 以下这块对的内容可以注释掉
+#   #这个地方需要关注, 之前未做此设置, 结果主备节点互相不能发现, 因此主备节点都升级成了MASTER, 并且绑定了VIP
+#   #主节点时, 内容为:
+#   #unicast_src_ip xxx.xxx.xxx.xxx
+#   #设置优先级, 确保主节点的优先级高过备用节点
+#   priority 100
+#   #用于设定主备节点间同步检查时间间隔
+#   advert_int 2
+#   #设置主备节点间的通信验证类型及密码, 同一个VRRP实例中需一致
+#   authentication {
+#     auth_type PASS
+#     auth_pass coderZsq
+#   }
+#   #集群资源监控, 组合vrrp_script进行
+#   track_script {
+#     check_haproxy
+#   }
+#   #设置虚拟IP地址, 当keepalived状态切换为MASTER时, 此IP会自动添加到系统中
+#   #当状态切换到BACKUP时, 此IP会自动从系统中删除
+#   #可以通过命令 ip addr查看切换后的状态
+#   virtual_ipaddress {
+#     172.19.189.100 #虚拟ip配置完之后就用它访问 需要同意网段
+#   }
+# }
+
+# 7. 启动keepalived进程
+systemctl start keepalived
+# [root@izuf69ibpdra8ui5a6edjtz keepalived]# ps -ef |grep keep
+# root       504     1  0 Apr08 ttyS0    00:00:00 /sbin/agetty --keep-baud 115200,38400,9600 ttyS0 vt220
+# root     22944     1  0 13:53 ?        00:00:00 /usr/local/keepalived/sbin/keepalived -D
+# root     22945 22944  0 13:53 ?        00:00:00 /usr/local/keepalived/sbin/keepalived -D
+# root     22951 22945  0 13:53 ?        00:00:00 /bin/bash /etc/keepalived/nginx_check.sh
+# root     22957 16142  0 13:53 pts/0    00:00:00 grep --color=auto keep
+
+# [root@izuf69ibpdra8ui5a6edjtz keepalived]# systemctl status keepalived
+# ● keepalived.service - LVS and VRRP High Availability Monitor
+#    Loaded: loaded (/usr/lib/systemd/system/keepalived.service; disabled; vendor preset: disabled)
+#    Active: active (running) since Thu 2020-05-21 13:53:38 CST; 15s ago
+#   Process: 22943 ExecStart=/usr/local/keepalived/sbin/keepalived $KEEPALIVED_OPTIONS (code=exited, status=0/SUCCESS)
+#  Main PID: 22944 (keepalived)
+#    CGroup: /system.slice/keepalived.service
+#            ├─22944 /usr/local/keepalived/sbin/keepalived -D
+#            └─22945 /usr/local/keepalived/sbin/keepalived -D
+
+# May 21 13:53:44 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:44 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:48 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Track script check_haproxy is already running, expect idle - skipping run
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: (PROXY) Sending/queueing gratuitous ARPs on eth0 for 172.19.189.100
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:49 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Sending gratuitous ARP on eth0 for 172.19.189.100
+# May 21 13:53:52 izuf69ibpdra8ui5a6edjtz Keepalived_vrrp[22945]: Track script check_haproxy is already running, expect idle - skipping run
+
+# [root@izuf69ibpdra8ui5a6edjtz keepalived]# ip addr
+# 1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN qlen 1
+#     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+#     inet 127.0.0.1/8 scope host lo
+#        valid_lft forever preferred_lft forever
+# 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
+#     link/ether 00:16:3e:0a:ff:87 brd ff:ff:ff:ff:ff:ff
+#     inet 172.19.189.121/20 brd 172.19.191.255 scope global eth0
+#        valid_lft forever preferred_lft forever
+#     inet 172.19.189.100/32 scope global eth0
+#        valid_lft forever preferred_lft forever
+
+# 6. 修改keepalived的BACKUP配置文件:
+# !Configuration File for keepalived
+# global_defs {
+#   router_id coderZsq #路由器标志
+# }
+# #集群资源监控, 组合track_script进行
+# vrrp_script check_haproxy {
+#   script "/etc/keepalived/nginx_check.sh" #检测 nginx 状态的脚本路径
+#   interval 2 #检测时间间隔
+#   weight -20 #条件成立 权重减20
+# }
+# vrrp_instance PROXY {
+#   #设置当前主机为主节点, 如果是备用节点, 则设置为BACKUP
+#   state BACKUP
+#   #指定HA监测网络接口, 可以用ifconfig查看来决定设置哪一个 ens32 / eth0
+#   interface eth0
+#   #虚拟路由标识, 同一个VRRP实例要使用同一个标识, 主备机
+#   virtual_router_id 80
+#   #因为当前环境中VRRP组播有问题, 改为使用单播发送VRRP报文 如果VRRP组播没问题, 以下这块对的内容可以注释掉
+#   #这个地方需要关注, 之前未做此设置, 结果主备节点互相不能发现, 因此主备节点都升级成了MASTER, 并且绑定了VIP
+#   #主节点时, 内容为:
+#   #unicast_src_ip xxx.xxx.xxx.xxx
+#   #设置优先级, 确保主节点的优先级高过备用节点
+#   priority 90
+#   #用于设定主备节点间同步检查时间间隔
+#   advert_int 2
+#   #设置主备节点间的通信验证类型及密码, 同一个VRRP实例中需一致
+#   authentication {
+#     auth_type PASS
+#     auth_pass coderZsq
+#   }
+#   #集群资源监控, 组合vrrp_script进行
+#   track_script {
+#     check_haproxy
+#   }
+#   #设置虚拟IP地址, 当keepalived状态切换为MASTER时, 此IP会自动添加到系统中
+#   #当状态切换到BACKUP时, 此IP会自动从系统中删除
+#   #可以通过命令 ip addr查看切换后的状态
+#   virtual_ipaddress {
+#     172.19.189.100 #虚拟ip配置完之后就用它访问 需要同意网段
+#   }
+# }
