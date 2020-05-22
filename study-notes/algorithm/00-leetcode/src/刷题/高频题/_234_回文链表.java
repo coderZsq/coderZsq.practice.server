a3d6d58f8f2ffd47ca9e699fb5819aa2e02528cf
coderZsq <a13701777868@yahoo.com>
2020-05-22 15:10:27 +0800

    add

--- study-notes/algorithm/00-leetcode/src/刷题/高频题/_234_回文链表.java
+++ study-notes/algorithm/00-leetcode/src/刷题/高频题/_234_回文链表.java
@@ -22,6 +22,42 @@ import 标签.链表.ListNode;
  */
 public class _234_回文链表 {
     public boolean isPalindrome(ListNode head) {
-        return false;
+        // 1. 如果节点为空或只有一个节点, 判定为回文链表
+        if (head == null || head.next == null) return true;
+        // 2. 如果有两个节点, 判断是否相等
+        if (head.next.next == null) return head.val == head.next.val;
+        // 3. 使用快慢指针获取中心节点, 中心节点判定为右边头节点的前驱节点
+        ListNode mid = middleNode(head);
+        // 4. 从右边头节点翻转链表
+        ListNode rHead = reverseList(mid.next);
+        // 5. 双指针头尾遍历
+        while (rHead != null) {
+            // 6. 遍历如果判定不同则不为回文链表
+            if (head.val != rHead.val) return false;
+            head = head.next;
+            rHead = rHead.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+        return newHead;
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
