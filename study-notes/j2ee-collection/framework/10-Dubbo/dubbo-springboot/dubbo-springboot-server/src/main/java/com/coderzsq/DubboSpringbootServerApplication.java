fa5d2590486f6df63fcb4a3ac6b4d6c08e501f8d
coderZsq <a13701777868@yahoo.com>
2020-05-30 00:07:16 +0800

    add

--- study-notes/j2ee-collection/framework/10-Dubbo/dubbo-springboot/dubbo-springboot-server/src/main/java/com/coderzsq/DubboSpringbootServerApplication.java
+++ study-notes/j2ee-collection/framework/10-Dubbo/dubbo-springboot/dubbo-springboot-server/src/main/java/com/coderzsq/DubboSpringbootServerApplication.java
@@ -1,11 +1,12 @@
 package com.coderzsq;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubboConfig
 @SpringBootApplication
 public class DubboSpringbootServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DubboSpringbootServerApplication.class, args);
     }
