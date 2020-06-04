6f76a5c777b2e140a005f7d9e80dd66c00311185
coderZsq <a13701777868@yahoo.com>
2020-06-04 13:15:31 +0800

    add

--- study-notes/algorithm/00-leetcode/src/刷题/高频题/_104_二叉树的最大深度.java
+++ study-notes/algorithm/00-leetcode/src/刷题/高频题/_104_二叉树的最大深度.java
@@ -12,16 +12,16 @@ import 标签.树.TreeNode;
  * 示例：
  * 给定二叉树 [3,9,20,null,null,15,7]，
  * <p>
- *     3
- *    / \
- *   9  20
- *  /     \
+ * 3
+ * / \
+ * 9  20
+ * /     \
  * 15      7
  * 返回它的最大深度 3 。
- *
- *  来源：力扣（LeetCode）
- *  链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
- *  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _104_二叉树的最大深度 {
     public int maxDepth(TreeNode root) {
--- study-notes/algorithm/00-leetcode/src/刷题/高频题/_155_最小栈.java
+++ study-notes/algorithm/00-leetcode/src/刷题/高频题/_155_最小栈.java
@@ -2,22 +2,22 @@ package 刷题.高频题;
 
 /**
  * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
- *
+ * <p>
  * push(x) —— 将元素 x 推入栈中。
  * pop() —— 删除栈顶的元素。
  * top() —— 获取栈顶元素。
  * getMin() —— 检索栈中的最小元素。
  *  
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入：
  * ["MinStack","push","push","push","getMin","pop","top","getMin"]
  * [[],[-2],[0],[-3],[],[],[],[]]
- *
+ * <p>
  * 输出：
  * [null,null,null,null,-3,null,0,-2]
- *
+ * <p>
  * 解释：
  * MinStack minStack = new MinStack();
  * minStack.push(-2);
@@ -28,11 +28,11 @@ package 刷题.高频题;
  * minStack.top();      --> 返回 0.
  * minStack.getMin();   --> 返回 -2.
  *  
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * pop、top 和 getMin 操作总是在 非空栈 上调用。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/min-stack
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
