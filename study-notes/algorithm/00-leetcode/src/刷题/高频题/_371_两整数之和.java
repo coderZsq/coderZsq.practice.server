be1b8a0872af63a9bfbf2f65a9670b1ae43ceb05
coderZsq <a13701777868@yahoo.com>
2020-06-05 14:32:33 +0800

    add

---
+++ study-notes/algorithm/00-leetcode/src/刷题/高频题/_371_两整数之和.java
@@ -0,0 +1,28 @@
+package 刷题.高频题;
+
+/**
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-two-integers/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _371_两整数之和 {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = (a & b) << 1;
+            a ^= b;
+            b = c;
+        }
+        return a;
+    }
+}
--- study-notes/algorithm/00-leetcode/src/标签/数组/_283_移动零.java
+++ study-notes/algorithm/00-leetcode/src/标签/数组/_283_移动零.java
@@ -18,3 +18,20 @@ public class _283_移动零 {
         }
     }
 }
+
+class _283_移动零2 {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        // 1. 双指针解法
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            // 2. 找到nums[j]不等于0为止
+            if (nums[j] == 0) continue;
+            // 3. 不等则交换
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            // 4. i进位
+            i++;
+        }
+    }
+}
