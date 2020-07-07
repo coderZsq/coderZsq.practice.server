22d29f5511552ceba04b52edb53c9acadb184a30
coderZsq <a13701777868@yahoo.com>
2020-07-07 12:20:53 +0800

    add

--- study-notes/j2ee-collection/java-web/07-Linux/rocketmq-install.sh
+++ study-notes/j2ee-collection/java-web/07-Linux/rocketmq-install.sh
@@ -82,3 +82,412 @@ vi conf/namesrv.conf
 # listenPort=9876
 # useEpollNativeSelector=true
 nohup ./mqnamesrv -c ../conf/namesrv.conf &
+
+# 集群搭建 3台服务器 需要JAVA环境
+# 调整运行内存 3台
+cd rocketmq-4.7.0/bin
+vi runbroker.sh
+# JAVA_OPT="${JAVA_OPT} -server -Xms2g -Xmx2g -Xmn1g"
+vi runserver.sh
+# JAVA_OPT="${JAVA_OPT} -server -Xms512m -Xmx512m -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m"
+
+# 启动三台节点的namesrv *3
+nohup bin/mqnamesrv &
+
+# 修改集群配置 
+cd conf/
+# 三主六从 异步
+mkdir 3m-6s-async
+cd 3m-6s-async
+
+# 第一台机器
+vi broker-a.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-a
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 0
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = ASYNC_MASTER
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-a/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10911
+
+# 第二台机器
+vi broker-b.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-b
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 0
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = ASYNC_MASTER
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-b/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10911
+
+# 第三台机器
+vi broker-c.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-c
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 0
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = ASYNC_MASTER
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-c/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10911
+
+# 启动三台 master broker
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c.properties &
+
+# 管理控制台
+scp application.properties rocketmq-console-ng-1.0.1.jar 7.101.146.105 :/root/
+vi application.properties
+# server.port=9999
+# rocketmq.config.namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+nohup java -jar rocketmq-console-ng-1.0.1.jar &
+
+# 配置第一台机器的2个从节点
+cd conf/3m-6s-async
+vi broker-b-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-b
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 1
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-b/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10811
+vi broker-c-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-c
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 1
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-c/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10711
+
+# 配置第二台机器的2个从节点
+cd conf/3m-6s-async
+vi broker-a-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-a
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 1
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-a/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10811
+vi broker-c-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-c
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 2
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-c/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10711
+
+# 配置第三台机器的2个从节点
+cd conf/3m-6s-async
+vi broker-a-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-a
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 2
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-a/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10811
+vi broker-b-s.properties
+# # 集群名字
+# brokerClusterName = DefaultCluster
+# # broker的组名字
+# brokerName = broker-b
+# # 0 代表Master, 非0 代表Slave
+# brokerId = 2
+# # 删除过期文件时间, 凌晨4点
+# deleteWhen = 04
+# # 文件保留时间48小时
+# fileReservedTime = 48
+# # broker的角色:  ASYNC_MASTER ,SYNC_MASTER ,SLAVE
+# brokerRole = SLAVE
+# # 数据刷盘模式 异步刷盘: ASYNC_FLUSH 同步刷盘: SYNC_FLUSH
+# flushDiskType = ASYNC_FLUSH
+# # broker的ip地址
+# brokerIP1=47.101.146.105 # 外网地址
+# brokerIP2=172.19.189.121 # 内网地址
+# # NameServer的地址信息 三台局域网地址
+# namesrvAddr=172.19.189.121:9876;172.19.189.121:9876;172.19.189.121:9876
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store
+# storePathCommitLog=/usr/local/rocketmq-4.7.0/data/broker-b/store/commitlog
+# # 是否自动创建主题, 建议关闭
+# autoCreateTopicEnable=false
+# # 动运行的监听端口
+# listenPort=10711
+
+# 启动六台 salve broker
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c-s.properties &
+
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c-s.properties &
+
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b-s.properties &
+
+# 在启动Broker的时候, 默认情况下我们指定的listenPort=10911, 但是在真正运行的时候, 会启动10911, 10909, 10912三个端口
+# haListenPort: 10912 在数据进行主从同步的端口(Master Slave之间通信的端口)
+# listenPort=10911 负责生产者和消费者和Broker进行连接通信
+# vip_Channel_port=10909 负责生产者 创建Topic, 发送Message的端口
+
+# 高可用集群Dledger
+vi broker-a.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-a
+# # 参与投票选举的通信操作
+# dLegerPeers=n0-172.19.189.121:40911;n1-172.19.189.121:40911;n2-172.19.189.121:40911
+# # 角色id, 必须唯一
+# dLegerSelfId=n0
+vi broker-a-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-a
+# # 参与投票选举的通信操作
+# dLegerPeers=n0-172.19.189.121:40911;n1-172.19.189.121:40911;n2-172.19.189.121:40911
+# # 角色id, 必须唯一
+# dLegerSelfId=n1
+vi broker-a-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-a/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-a
+# # 参与投票选举的通信操作
+# dLegerPeers=n0-172.19.189.121:40911;n1-172.19.189.121:40911;n2-172.19.189.121:40911
+# # 角色id, 必须唯一
+# dLegerSelfId=n2
+
+vi broker-b.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-b
+# # 参与投票选举的通信操作
+# dLegerPeers=n4-172.19.189.121:40811;n5-172.19.189.121:40811;n6-172.19.189.121:40811
+# # 角色id, 必须唯一
+# dLegerSelfId=n4
+vi broker-b-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-b
+# # 参与投票选举的通信操作
+# dLegerPeers=n4-172.19.189.121:40811;n5-172.19.189.121:40811;n6-172.19.189.121:40811
+# # 角色id, 必须唯一
+# dLegerSelfId=n5
+vi broker-b-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-b/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-b
+# # 参与投票选举的通信操作
+# dLegerPeers=n4-172.19.189.121:40811;n5-172.19.189.121:40811;n6-172.19.189.121:40811
+# # 角色id, 必须唯一
+# dLegerSelfId=n6
+
+vi broker-c.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-c
+# # 参与投票选举的通信操作
+# dLegerPeers=n7-172.19.189.121:40711;n8-172.19.189.121:40711;n9-172.19.189.121:40711
+# # 角色id, 必须唯一
+# dLegerSelfId=n7
+vi broker-c-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-c
+# # 参与投票选举的通信操作
+# dLegerPeers=n7-172.19.189.121:40711;n8-172.19.189.121:40711;n9-172.19.189.121:40711
+# # 角色id, 必须唯一
+# dLegerSelfId=n8
+vi broker-c-s.properties
+# # 累加配置
+# # 把commitLog交给Dleger高可用管理
+# enableDLegerCommitLog=true
+# storePathRootDir=/usr/local/rocketmq-4.7.0/data/broker-c/store/dledger_store
+# # 对于不同的broker需要添加不不同的组别
+# dLegerGroup=broker-c
+# # 参与投票选举的通信操作
+# dLegerPeers=n7-172.19.189.121:40711;n8-172.19.189.121:40711;n9-172.19.189.121:40711
+# # 角色id, 必须唯一
+# dLegerSelfId=n9
+
+# 启动高可用 broker
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c-s.properties &
+
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c-s.properties &
+
+nohup bin/mqbroker -c conf/3m-6s-async/broker-c.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-a-s.properties &
+nohup bin/mqbroker -c conf/3m-6s-async/broker-b-s.properties &
