1f8abe83261d3538d4c85599513a3e6c61031d04
coderZsq <a13701777868@yahoo.com>
2020-08-24 18:02:14 +0800

    add

--- study-notes/j2ee-collection/framework/00-项目驱动/13_Spring06_整合MyBatis/src/main/java/com/sq/dao/SkillDao.java
+++ study-notes/j2ee-collection/framework/00-项目驱动/13_Spring06_整合MyBatis/src/main/java/com/sq/dao/SkillDao.java
@@ -1,9 +1,13 @@
 package com.sq.dao;
 
 import com.sq.domain.Skill;
+import org.apache.ibatis.annotations.Insert;
 
 import java.util.List;
 
 public interface SkillDao {
     List<Skill> list();
+
+    @Insert("INSERT INTO skill(name, level) VALUES (#{name}, #{level})")
+    boolean save(Skill skill);
 }
