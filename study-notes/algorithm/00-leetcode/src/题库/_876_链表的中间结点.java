8869a9c9f82bd9efdaeb5f2362f757c5a9e16c8e
coderZsq <a13701777868@yahoo.com>
2020-09-27 19:19:22 +0800

    add

--- study-notes/algorithm/00-leetcode/src/题库/_876_链表的中间结点.java
+++ study-notes/algorithm/00-leetcode/src/题库/_876_链表的中间结点.java
@@ -12,7 +12,7 @@ public class _876_链表的中间结点 {
         if (head == null) return null;
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next == null) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
