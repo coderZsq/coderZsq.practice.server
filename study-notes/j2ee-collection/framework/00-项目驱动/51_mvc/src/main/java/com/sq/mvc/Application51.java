d95ca0b9263c7a1dfe6ca712281b431efbb9cadb
coderZsq <a13701777868@yahoo.com>
2020-11-24 12:41:10 +0800

    add

---
+++ study-notes/j2ee-collection/framework/00-项目驱动/51_mvc/src/main/java/com/sq/mvc/Application51.java
@@ -0,0 +1,11 @@
+package com.sq.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application51 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application51.class, args);
+    }
+}
