7db6a95fd9d511fe2a3e41383c4fb78589549106
coderZsq <a13701777868@yahoo.com>
2020-11-24 13:43:48 +0800

    add

--- study-notes/j2ee-collection/framework/00-项目驱动/51_mvc/src/main/java/com/sq/mvc/cfg/SpringMvcConfig.java
+++ study-notes/j2ee-collection/framework/00-项目驱动/51_mvc/src/main/java/com/sq/mvc/cfg/SpringMvcConfig.java
@@ -1,9 +1,17 @@
 package com.sq.mvc.cfg;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
-
+    // @Override
+    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // registry.addResourceHandler("/abc1/**")
+        //         .addResourceLocations("classpath:/static/");
+        //
+        // registry.addResourceHandler("/abc2/**")
+        //         .addResourceLocations("file:///Users/zhushuangquan/Desktop/upload/");
+    // }
 }
--- study-notes/j2ee-collection/framework/00-项目驱动/51_mvc/src/main/java/com/sq/mvc/controller/TestController.java
+++ study-notes/j2ee-collection/framework/00-项目驱动/51_mvc/src/main/java/com/sq/mvc/controller/TestController.java
@@ -3,14 +3,19 @@ package com.sq.mvc.controller;
 import com.sq.mvc.prop.ProjectProperties;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.ClassLoaderObjectInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.UUID;
 
@@ -47,5 +52,19 @@ public class TestController {
         // /private/var/folders/dr/q415vqjx46n40jw2m8htjfgm0000gn/T/tomcat-docbase.17039982073840826461.8080/
 
         return "上传成功!!!";
+    }
+
+    @GetMapping("/download")
+    public void download(HttpServletResponse response) throws Exception {
+        // 把文件以附件的形式返回
+
+        // 设置响应头
+        response.setHeader("Content-Disposition", "attachment; filename=test.txt");
+
+        // 读取文件
+        try (InputStream is = new ClassPathResource("static/txt/test.txt").getInputStream()) {
+            // 将文件数据利用response写回到客户端
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
