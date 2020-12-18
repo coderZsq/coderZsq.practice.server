c1eb50e4e6f373a4e6360da6ae59b2e21bc60188
coderZsq <a13701777868@yahoo.com>
2020-12-18 16:43:15 +0800

    add

--- study-notes/j2ee-collection/architecture/04-Java并发/concurrent-utils/src/main/java/com/sq/demo/countdownlatch/CountDownLatchDemo01.java
+++ study-notes/j2ee-collection/architecture/04-Java并发/concurrent-utils/src/main/java/com/sq/demo/countdownlatch/CountDownLatchDemo01.java
@@ -1,4 +1,35 @@
 package com.sq.demo.countdownlatch;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class CountDownLatchDemo01 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 创建一个计数器对象, 并且赋值为2
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+        /**
+         * 创建两个线程
+         */
+        for (int i = 0; i < 2; i++) {
+            new Thread("T"+ i) {
+                @Override
+                public void run() {
+                    System.out.println("当前线程" + Thread.currentThread().getName() + "开始进行数据分析处理");
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                        System.out.println("当前线程" + Thread.currentThread().getName() + "数据分析处理完毕");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        countDownLatch.countDown(); // 计数器-1
+                    }
+                }
+            }.start();
+        }
+        // 阻塞当前线程, 当计数器为0的时候, 会唤醒当前线程
+        countDownLatch.await();
+        // 汇总处理之后的结果
+        System.out.println("所有的任务线程都已经处理完毕, main 线程: 开始进行所有的数据汇总");
+    }
 }
---
+++ study-notes/j2ee-collection/architecture/04-Java并发/concurrent-utils/src/main/java/com/sq/demo/countdownlatch/CountDownLatchDemo02.java
@@ -0,0 +1,80 @@
+package com.sq.demo.countdownlatch;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 1 使用多线程的方式 读取指定文件夹里面所有的txt文件, 统计每个单词出现的数量
+ * 2 统计完成后, 最后汇总每个单词出现的次数
+ * <p>
+ * a 使用线程池 ThreadPoolExecutor
+ * b 字符文件输入流 BufferedReader
+ * c 使用Map保存对应的数据
+ */
+public class CountDownLatchDemo02 {
+    public static void main(String[] args) throws InterruptedException {
+        // 1 创建一个线程池
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(16, 16, 0, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
+        // 3 定义一个list, 用来保存所有任务的一个结果
+        final ConcurrentLinkedDeque<HashMap<String, Integer>> list = new ConcurrentLinkedDeque<>();
+        // 4 定义读取文件的目录
+        File dir = new File("/Users/zhushuangquan/Codes/GitHub/coderZsq.practice.server/study-notes/j2ee-collection/architecture/04-Java并发/concurrent-utils/src/main/resources");
+        // 获取到所有文件
+        final File[] files = dir.listFiles();
+        final int count = files.length;
+        for (int i = 0; i < count; i++) {
+            executor.execute(new TaskInfo(files[i], list));
+        }
+
+        // 汇总结果
+        final HashMap<String, Integer> total = new HashMap<>();
+        for (HashMap<String, Integer> hashMap : list) {
+            for (String key : hashMap.keySet()) {
+                final Integer oldValue = total.getOrDefault(key, 0);
+                total.put(key, oldValue + hashMap.get(key));
+            }
+        }
+        System.out.println("所有的数据汇总结果: " + total);
+    }
+
+    // 2 定义需要执行的任务 Runnable接口
+    static class TaskInfo implements Runnable {
+        // 需要读取的文件
+        File file = null;
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        ConcurrentLinkedDeque<HashMap<String, Integer>> list = null;
+
+        public TaskInfo(File file, ConcurrentLinkedDeque<HashMap<String, Integer>> list) {
+            this.file = file;
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            // 创建文件流对象读取文件
+            try (
+                    FileReader fr = new FileReader(file);
+                    BufferedReader br = new BufferedReader(fr)
+            ) {
+                String line = null; // 读取到文件内容
+                while ((line = br.readLine()) != null) {
+                    final String[] words = line.split(" ");
+                    for (String key : words) {
+                        final Integer oldValue = hashMap.getOrDefault(key, 0);
+                        hashMap.put(key, oldValue + 1);
+                    }
+                }
+                list.add(hashMap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
