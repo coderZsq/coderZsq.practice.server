a8204945e11ceacc741180131456509c312dd05e
coderZsq <a13701777868@yahoo.com>
2020-12-25 17:58:21 +0800

    add

---
+++ study-notes/j2ee-collection/architecture/05-JVM性能调优/compile.sh
@@ -0,0 +1,18 @@
+$ javac Main.java
+
+$ javap -c Main.class
+
+# Compiled from "Main.java"
+# public class Main {
+#   public Main();
+#     Code:
+#        0: aload_0
+#        1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+#        4: return
+
+#   public static void main(java.lang.String[]);
+#     Code:
+#        0: iconst_1
+#        1: istore_1
+#        2: return
+# }
