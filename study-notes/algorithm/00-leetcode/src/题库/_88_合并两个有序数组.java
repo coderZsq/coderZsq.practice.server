39baf004541811bfceecdfa83adad54961042b54
coderZsq <a13701777868@yahoo.com>
2021-01-04 13:20:59 +0800

    add

--- study-notes/algorithm/00-leetcode/src/题库/_88_合并两个有序数组.java
+++ study-notes/algorithm/00-leetcode/src/题库/_88_合并两个有序数组.java
@@ -19,3 +19,35 @@ public class _88_合并两个有序数组 {
         }
     }
 }
+
+class _88_合并两个有序数组2 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int cur = nums1.length - 1;
+        while (i1 >= 0 && i2 >= 0) {
+            if (nums1[i1] < nums2[i2]) {
+                nums1[cur] = nums2[i2];
+                i2--;
+                cur--;
+            } else {
+                nums1[cur] = nums1[i1];
+                i1--;
+                cur--;
+            }
+        }
+        if (i1 < 0) {
+            while (i2 >= 0) {
+                nums1[cur] = nums2[i2];
+                cur--;
+                i2--;
+            }
+        } else {
+            while (i1 >= 0) {
+                nums1[cur] = nums1[i1];
+                cur--;
+                i1--;
+            }
+        }
+    }
+}
