b742344a39edc83d7a0e683a9c639eafa131791d
coderZsq <a13701777868@yahoo.com>
2021-01-26 14:43:03 +0800

    add

---
+++ study-notes/algorithm/00-leetcode/src/标签/数组/_1128_等价多米诺骨牌对的数量.java
@@ -0,0 +1,23 @@
+package 标签.数组;
+
+/**
+ * https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/
+ *
+ * @author zhushuangquan
+ */
+public class _1128_等价多米诺骨牌对的数量 {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] bucket = new int[100];
+        int ret = 0;
+        for (int[] dominoe : dominoes) {
+            int val;
+            if (dominoe[0] < dominoe[1]) {
+                val = dominoe[0] * 10 + dominoe[1];
+            } else {
+                val = dominoe[1] * 10 + dominoe[0];
+            }
+            ret += bucket[val]++;
+        }
+        return ret;
+    }
+}
