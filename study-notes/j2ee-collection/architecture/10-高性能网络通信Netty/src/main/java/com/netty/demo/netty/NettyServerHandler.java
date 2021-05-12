c7a416917aa05f44ce6af4ea3670c623a5eb779c
coderZsq <a13701777868@yahoo.com>
2021-05-12 17:58:57 +0800

    add

---
+++ study-notes/j2ee-collection/architecture/10-高性能网络通信Netty/src/main/java/com/netty/demo/netty/NettyServerHandler.java
@@ -0,0 +1,22 @@
+package com.netty.demo.netty;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+public class NettyServerHandler implements ChannelHandler {
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+}
