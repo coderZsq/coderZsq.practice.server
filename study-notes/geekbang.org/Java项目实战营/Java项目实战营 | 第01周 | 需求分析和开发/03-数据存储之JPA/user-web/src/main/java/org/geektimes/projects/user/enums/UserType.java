1e96383388538aac778116acfafac713b821c05d
coderZsq <a13701777868@yahoo.com>
2021-08-05 14:44:00 +0800

    add

---
+++ study-notes/geekbang.org/Java项目实战营/Java项目实战营 | 第01周 | 需求分析和开发/03-数据存储之JPA/user-web/src/main/java/org/geektimes/projects/user/enums/UserType.java
@@ -0,0 +1,14 @@
+package org.geektimes.projects.user.enums;
+
+public enum UserType { // 底层实际 public final class UserType extends java.lang.Enum
+    NORMAL,
+    VIP;
+
+    UserType() { // 枚举中构造器是private
+
+    }
+
+    public static void main(String[] args) {
+        UserType.VIP.ordinal();
+    }
+}
