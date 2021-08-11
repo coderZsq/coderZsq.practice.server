90fffc47ef2b07e46b674969aa6f93dd1e380bb7
coderZsq <a13701777868@yahoo.com>
2021-08-11 16:49:56 +0800

    add

---
+++ study-notes/geekbang.org/Java项目实战营/Java项目实战营 | 第03周 | 分布式架构设计/09-同步服务/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -0,0 +1,30 @@
+package org.geektimes.projects.user.web.listener;
+
+import org.geektimes.context.ClassicComponentContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * {@link ClassicComponentContext} 初始化器
+ * ContextLoaderListener
+ */
+public class ComponentContextInitializerListener implements ServletContextListener {
+
+    private ServletContext servletContext;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        this.servletContext = sce.getServletContext();
+        ClassicComponentContext context = new ClassicComponentContext();
+        context.init(servletContext);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+//        ComponentContext context = ComponentContext.getInstance();
+//        context.destroy();
+    }
+
+}
