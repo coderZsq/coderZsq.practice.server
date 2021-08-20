dbded1a46db8387b2e67b80f04576cc93c762e2e
coderZsq <a13701777868@yahoo.com>
2021-08-20 17:21:55 +0800

    add

---
+++ study-notes/geekbang.org/视频课/小马哥讲Spring核心编程思想/163 | 自定义PropertyEditor扩展：不尝试怎么知道它好不好用？/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/MyDestructionAwareBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package org.geekbang.thinking.in.spring.bean.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * {@link DestructionAwareBeanPostProcessor} 实现
+ */
+public class MyDestructionAwareBeanPostProcessor implements DestructionAwareBeanPostProcessor {
+
+    @Override
+    public void postProcessBeforeDestruction(Object bean, String beanName) throws BeansException {
+        if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+            UserHolder userHolder = (UserHolder) bean;
+            // afterSingletonsInstantiated() = The user holder V8
+            // UserHolder description = "The user holder V8"
+            userHolder.setDescription("The user holder V9");
+            System.out.println("postProcessBeforeDestruction() : " + userHolder.getDescription());
+        }
+    }
+}
