8c489e7caf2641cbb733d0ba7d81b2171aa5a350
coderZsq <a13701777868@yahoo.com>
2021-08-31 15:20:56 +0800

    add

---
+++ study-notes/geekbang.org/视频课/小马哥讲Spring AOP编程思想/10 | Java AOP拦截器模式（Interceptor）：拦截执行分别代表什么？/aop-overview/src/main/java/org/geekbang/thinking/in/spring/aop/overview/JdkDynamicProxyDemo.java
@@ -0,0 +1,28 @@
+package org.geekbang.thinking.in.spring.aop.overview;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * JDK 动态代理示例
+ */
+public class JdkDynamicProxyDemo {
+
+    public static void main(String[] args) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Object proxy = Proxy.newProxyInstance(classLoader, new Class[]{EchoService.class}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (EchoService.class.isAssignableFrom(method.getDeclaringClass())) {
+                    ProxyEchoService echoService = new ProxyEchoService(new DefaultEchoService());
+                    return echoService.echo((String) args[0]);
+                }
+                return null;
+            }
+        });
+
+        EchoService echoService = (EchoService) proxy;
+        echoService.echo("Hello,World");
+    }
+}
---
+++ study-notes/geekbang.org/视频课/小马哥讲Spring AOP编程思想/17 | CGLIB动态代理：为什么Java动态代理无法满足AOP的需要？/annotation/src/main/java/org/geekbang/thinking/in/spring/annotation/HelloWorldImportBeanDefinitionRegistrar.java
@@ -0,0 +1,23 @@
+package org.geekbang.thinking.in.spring.annotation;
+
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * "HelloWorld" 模块 {@link ImportBeanDefinitionRegistrar}
+ *
+ * @see ImportBeanDefinitionRegistrar
+ */
+public class HelloWorldImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+                                        BeanDefinitionRegistry registry) {
+        AnnotatedGenericBeanDefinition beanDefinition = new AnnotatedGenericBeanDefinition(HelloWorldConfiguration.class);
+        BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinition, registry);
+    }
+}
