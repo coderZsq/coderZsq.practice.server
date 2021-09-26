9c7bfc284873e6f07ef58afea43a7fe18d05a4f9
coderZsq <a13701777868@yahoo.com>
2021-09-26 15:00:15 +0800

    add

---
+++ study-notes/geekbang.org/视频课/Go语言从入门到实战/13 | Go语言的函数/src/ch10/func/func_test.go
@@ -0,0 +1,33 @@
+package fu_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFn(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSF := timeSpent(slowFn)
+	t.Log(tsSF(10))
+}
